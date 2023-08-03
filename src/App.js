903016d7ef49931d7fd029231e7099ed7cef751a
UjjwalSingh <30306011+Ujjwal1629@users.noreply.github.com>
2023-08-03 13:33:25 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import "./App.css";
 import MainContent from "./components/MainContent";
 import Header from "./components/Header";
 
+// Approach for Solving the Problem
+// 1. Created a new React app named it as Machine-Test
+// 2. Created a Header component(for navbar) stored it inside the components folder
+// 3. Created a MainContent component(for main page content) stored it inside the components folder
+// 4. Fetched the data from the API(using nested data function)
+// 5. Displayed the data in the MainContent component
+// 6. exported the components and imported them in the App.js file
+// 7. Style the Header and MainContent components
+// 8. Deployed the app to vercel to showcase on the live server
+
 function App() {
   return (
     <div className="App">
